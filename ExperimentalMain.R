1b1ebb8478a3b492f0f58fea2520d7ffb06ce7aa
Maria Laura <marialauradelvallemarcos@gmail.com>
2015-10-18 19:53:45 -0300

    agregué lo que faltaba para usar las nuevas funciones

--- ExperimentalMain.R
+++ ExperimentalMain.R
@@ -3,220 +3,176 @@
 # El programa calcula medidas de variabilidad estructural y dinámica de alineamientos múltiples de 
 # familias de proteínas provenientes de la base de datos de alineamientos múltiples "Homstrad".
 #
-# Para utilizar el programa se debe completar un input ("DATA/Experimental/inputE.csv") especificando: 
-# -family:la familia de proteínas del alineamiento, que puede ser "SerinProteases", "Globins" o "Plastocyanins"
+# Para utilizar el programa se debe completar un input ("DATA/Experimental/experimental_input.csv") especificando: 
+# -family:la familia de proteínas del alineamiento, que puede ser "serinProteases", "globins" o "plastocyanins"
 # -p.ref: la proteína de referncia que se desee utilizar, teniendo en cuenta que la misma debe ser escrita de 
 # la misma forma en la que se encuentra en el dataset de la familia correspondiente 
-# (ej:"DATA/Experimental/SerinProteasesDataset.csv").
+# (ej:"DATA/Experimental/serinProteases_dataset.csv").
 # -heme: solo se utiliza para la familia de las globinas, puede ser "TRUE" o "FALSE", dependiendo de si 
 # se desea o no considerar al grupo HEMO. 
 # -model: el modelo de red elástica a usar, que solo puede ser "ANM" por el momento.
 # -R0: cut-off del ANM.
-
-#Remove objects#
+# Remove objects.
 rm(list = ls())
-
-#Read input#
+# Read input.
 data.dir <- "DATA/Experimental"
-
-input.dir <- file.path(data.dir,"InputE.csv")
-input <- read.csv(input.dir)
-
-#Reference family and protein#
+input.fname <- file.path(data.dir, "experimental_input.csv")
+input <- read.csv(input.fname)
 family <- input$family
 p.ref <- input$p.ref 
-heme = input$heme
-
-#ENM#
+heme <- input$heme
 model <- input$model
 R0 = input$R0
-
-#Files dir#
-align.dir <- file.path(data.dir,paste(family,"Alignments.txt",sep="")) #File with a multiple sequence alignmet#
-pdbs.dir <- file.path(data.dir,paste(family,"Coordinates.pdb",sep="")) #Coordinates of the proteins# 
-dataset.dir <- file.path(data.dir,paste(family,"Dataset.csv",sep="")) #Dataset with pdbids and chains# 
-
-#Functions dir#
-f.readCA <-"FUNCTIONS/ReadCA.R" 
-f.readHeme <- "FUNCTIONS/ReadHeme.R"  
-f.alignment <- "FUNCTIONS/Alignment.R" 
-f.ID <- "FUNCTIONS/ID.R" 
-f.Keff <- "FUNCTIONS/Keff.R"  
-f.K <- "FUNCTIONS/K.R"  
-f.kij.function <- file.path("FUNCTIONS",paste("Kij.",model,".R",sep = "")) 
-f.variability <- "FUNCTIONS/Variability.R" 
-
-#Output dir#
-OUT <- "OUT/Experimental"
-
-#General parameters#
+# Files fnames.
+alignment.fname <- file.path(data.dir, paste(family, "_alignment.txt", sep = ""))  # File with the multiple sequence alignmet.
+pdbs.fname <- file.path(data.dir, paste(family, "_coordinates.pdb", sep = ""))  # Coordinates of the proteins.
+dataset.fname <- file.path(data.dir, paste(family, "_dataset.csv", sep = ""))  # Dataset with pdbids and chains.
+# Functions fnames.
+ReadCA.fname <- "FUNCTIONS/ReadCA.R" 
+ReadHeme.fname <- "FUNCTIONS/ReadHeme.R"  
+AnalyzeAlignment.fname <- "FUNCTIONS/AnalyzeAlignment.R" 
+CalculateID.fname <- "FUNCTIONS/CalculateID.R" 
+CalculateKeff.fname <- "FUNCTIONS/CalculateKeff.R"  
+CalculateK.fname <- "FUNCTIONS/CalculateK.R"  
+CalculateKij.fname <- file.path("FUNCTIONS", paste("CalculateKij", model, ".R", sep = "")) 
+CalculateVariability.fname <- "FUNCTIONS/CalculateVariability.R" 
+# Output dir.
+out.dir <- "OUT/Experimental"
+# General parameters.
 TOLERANCE = 1e-10 
-
-#Load Librarys#
+# Load Librarys.
 library(seqinr) 
 library(bio3d) 
-
-#Load Functions#
-source(f.readCA) 
-source(f.readHeme) 
-source(f.alignment)
-source(f.ID)
-source(f.Keff)
-source(f.K)
-source(f.kij.function)
-source(f.variability)
-
-#Read Dataset#
-Dataset <- read.csv(dataset.dir)
-pdbids <- Dataset$pdbid
-chains <- Dataset$chain
+# Source Functions.
+source(ReadCA.fname) 
+source(ReadHeme.fname) 
+source(AnalyzeAlignment.fname)
+source(CalculateID.fname)
+source(CalculateKeff.fname)
+source(CalculateK.fname)
+source(CalculateKij.fname)
+source(CalculateVariability.fname)
+# Read dataset.
+dataset <- read.csv(dataset.fname)
+pdbids <- dataset$pdbid
+chains <- dataset$chain
 nprot = length(pdbids)
-
-#Read multiple alignment#
-alignments.id <- read.fasta(align.dir)
-alignments <- alignments.id$ali[,-ncol(alignments.id$ali)]
-
-#Read PDB & alignment of p.ref#
+# Read multiple alignment.
+alignments.ids <- read.fasta(alignment.fname)
+alignments <- alignments.ids$ali[, -ncol(alignments.ids$ali)]
+# Read PDB & alignment of p.ref.
 chain.p.ref <- chains[pdbids == as.character(p.ref)]
-pdb.p.ref <- readCA(pdbs.dir,chain.p.ref)
+pdb.p.ref <- ReadCA(pdbs.fname, chain.p.ref)
 r.p.ref = pdb.p.ref$xyz.calpha
 nsites.p.ref = pdb.p.ref$nsites 
-naa.p.ref = nsites.p.ref #naa = n.aminoaids, nsites = n.aminoácids + n.atoms we want to consider, ej.: Ns and Fe from heme group in Globins# 
-
-if ((family == "Globins") & (heme == "TRUE")){
-  r.heme.p.ref = readHeme(pdbs.dir,chain.p.ref)
-  r.p.ref = cbind(r.p.ref,r.heme.p.ref)
+naa.p.ref = nsites.p.ref 
+if (family == "globins" & heme == "TRUE") {
+  r.heme.p.ref = ReadHeme(pdbs.fname, chain.p.ref)
+  r.p.ref = cbind(r.p.ref, r.heme.p.ref)
   nsites.p.ref = ncol(r.p.ref)
 }
-
-alignment.p.ref <- alignments[alignments.id$id == as.character(p.ref),]
-
-#Variability#
-m.lalign = matrix(ncol = 1 , nrow = nprot)
-m.ID = matrix(ncol = 1 , nrow = nprot)
-m.nH = matrix(ncol = 3*naa.p.ref , nrow = nprot)
-m.Pn = matrix(ncol = 3*naa.p.ref , nrow = nprot)
-m.evalues = matrix(ncol = 3*naa.p.ref , nrow = nprot)
-m.d.evalues = matrix(ncol = 3*naa.p.ref , nrow = nprot)
-m.dr.squarei = matrix(ncol = naa.p.ref , nrow = nprot)
-
-for (P in (1:nprot)){
-  
-	#Read PDB & alignment of p.2#
+alignment.p.ref <- alignments[alignments.ids$id == as.character(p.ref), ]
+# Measures of variability.
+m.laligned = matrix(ncol = 1, nrow = nprot)
+m.ID = matrix(ncol = 1, nrow = nprot)
+m.nH = matrix(ncol = 3 * naa.p.ref, nrow = nprot)
+m.Pn = matrix(ncol = 3 * naa.p.ref, nrow = nprot)
+m.evalues = matrix(ncol = 3 * naa.p.ref, nrow = nprot)
+m.d.evalues = matrix(ncol = 3 * naa.p.ref, nrow = nprot)
+m.dr.squarei = matrix(ncol = naa.p.ref, nrow = nprot)
+for (P in (1:nprot)) {
+	# Read PDB & alignment of p.2.
 	chain.p.2 <- chains[[P]]
-	pdb.p.2 <- readCA(pdbs.dir,chain.p.2)
+	pdb.p.2 <- ReadCA(pdbs.fname, chain.p.2)
 	r.p.2 <- pdb.p.2$xyz.calpha
 	nsites.p.2 <- pdb.p.2$nsites
 	naa.p.2 = nsites.p.2
-	
-	if ((family == "Globins") & (heme == "TRUE")){
-	  r.heme.p.2 = readHeme(pdbs.dir,chain.p.2)
-	  r.p.2 = cbind(r.p.2,r.heme.p.2)
+	if (family == "globins" & heme == "TRUE") {
+	  r.heme.p.2 = ReadHeme(pdbs.fname, chain.p.2)
+	  r.p.2 = cbind(r.p.2, r.heme.p.2)
 	  nsites.p.2 = ncol(r.p.2)
 	}
-
-	alignment.p.2 <- alignments[alignments.id$id == as.character(pdbids[P]),]	
-	
-	#Analyze alignment of p.ref#
-	a.align.p.ref <- alignment(alignment.p.ref,alignment.p.2,naa.p.ref)
-	align.p.ref.index <- a.align.p.ref$align.index
-	nalign <- a.align.p.ref$nalign
-	r.align.p.ref <- r.p.ref[,align.p.ref.index]
-	no.align.p.ref.index <- a.align.p.ref$no.align.index
-	
-	if ((family == "Globins") & (heme == "TRUE")){
-	  no.align.p.ref.index <- cbind(no.align.p.ref.index,t(seq((naa.p.ref+1),nsites.p.ref)))
+	alignment.p.2 <- alignments[alignments.ids$id == as.character(pdbids[P]), ]	
+	# Calculate %ID between P.ref y p.2.
+	ID.p.2 <- CalculateID(alignment.p.ref, alignment.p.2)
+	# Analyze alignment of p.ref.
+	a.alignment.p.ref <- AnalyzeAlignment(alignment.p.ref, alignment.p.2, naa.p.ref)
+	aligned.p.ref.index <- a.alignment.p.ref$aligned.index
+	naligned <- a.alignment.p.ref$naligned
+	not.aligned.p.ref.index <- a.alignment.p.ref$not.aligned.index
+	# Analyze alignment of p.2.
+	a.alignment.p.2 <- AnalyzeAlignment(alignment.p.2, alignment.p.ref, naa.p.2)
+	aligned.p.2.index <- a.alignment.p.2$aligned.index
+	not.aligned.p.2.index <- a.alignment.p.2$not.aligned.index
+	# Add heme to not.aligned.
+	if (family == "globins" & heme == "TRUE"){
+	  not.aligned.p.ref.index <- cbind(not.aligned.p.ref.index, t(seq((naa.p.ref+1), nsites.p.ref)))
+	  not.aligned.p.2.index <- cbind(not.aligned.p.2.index, t(seq((naa.p.2+1), nsites.p.2)))
 	}
-	
-	#Cakculate KEFF p.ref#
-	Keff.p.ref <- keff(r.p.ref,align.p.ref.index,no.align.p.ref.index,kij.function,R0,TOLERANCE)	
-	nmodes <- length(Keff.p.ref$va)
-	
-	#Calculate %ID between P.ref y p.2#
-	ID.p.2 <- ID(alignment.p.ref,alignment.p.2)
-	
-	#Analyze alignment of p.2#
-	a.align.p.2 <- alignment(alignment.p.2,alignment.p.ref,naa.p.2)
-	align.p.2.index <- a.align.p.2$align.index
-	no.align.p.2.index <- a.align.p.2$no.align.index
-	
-	if ((family == "Globins") & (heme == "TRUE")){
-	  no.align.p.2.index <- cbind(no.align.p.2.index,t(seq((naa.p.2+1),nsites.p.2)))
-	}
-	
-	#Rotate p.2 minimizing RMSD with P.ref#
-	align.p.ref.index3N = matrix(0 , ncol = 3*nalign, nrow = 1)
-	align.p.2.index3N = matrix(0 , ncol = 3*nalign, nrow = 1)
-  for (i in (1: nalign)){
-    align.p.ref.index3N[1,((3*i-2):(3*i))] = c((3*align.p.ref.index[i]-2),(3*align.p.ref.index[i]-1),(3*align.p.ref.index[i]))
-    align.p.2.index3N[1,((3*i-2):(3*i))] = c((3*align.p.2.index[i]-2),(3*align.p.2.index[i]-1),(3*align.p.2.index[i]))
+	# Rotate r.p.2 minimizing RMSD with P.ref.
+	aligned.p.ref.index3N = matrix(0, ncol = 3 * naligned, nrow = 1)
+	aligned.p.2.index3N = matrix(0, ncol = 3 * naligned, nrow = 1)
+  for (i in (1:naligned)) {
+    aligned.p.ref.index3N[1, ((3 * i - 2):(3 * i))] = c((3 * aligned.p.ref.index[i] - 2), (3 * aligned.p.ref.index[i] - 1),(3 * aligned.p.ref.index[i]))
+    aligned.p.2.index3N[1, ((3 * i - 2):(3 * i))] = c((3 * aligned.p.2.index[i] - 2), (3 * aligned.p.2.index[i] - 1),(3 * aligned.p.2.index[i]))
   }
-
 	r.p.2 <- matrix(fit.xyz(fixed = as.vector(r.p.ref),
-	mobile = as.vector(r.p.2),
-	fixed.inds = align.p.ref.index3N,
-  mobile.inds = align.p.2.index3N
-	),nrow=3)
-	
-	r.align.p.2 <- r.p.2[,align.p.2.index]
-	
-	#Cakculate KEFF p.2#	
-	Keff.p.2 <- keff(r.p.2,align.p.2.index,no.align.p.2.index,kij.function,R0,TOLERANCE)	
-	
-	#dr#	
-	dr = r.align.p.2 - r.align.p.ref
-
-	#Variability#
-	VA <- variability(dr,Keff.p.ref,Keff.p.2)
+	                       mobile = as.vector(r.p.2),
+	                   fixed.inds = aligned.p.ref.index3N,
+                    mobile.inds = aligned.p.2.index3N), nrow = 3)
+	# Get aligned coordinates.
+	r.aligned.p.ref <- r.p.ref[, aligned.p.ref.index]
+	r.aligned.p.2 <- r.p.2[, aligned.p.2.index]
+	# Calculate dr.
+	dr = r.aligned.p.2 - r.aligned.p.ref
+	# Cakculate KEFF p.ref.
+	Keff.p.ref <- CalculateKeff(r.p.ref, aligned.p.ref.index, not.aligned.p.ref.index, CalculateKij, R0, TOLERANCE)	
+	nmodes <- length(Keff.p.ref$va)
+	# Calculate KEFF p.2.
+	Keff.p.2 <- CalculateKeff(r.p.2, aligned.p.2.index, not.aligned.p.2.index, CalculateKij, R0, TOLERANCE)	
+	# Calculate measures of variability.
+	VA <- CalculateVariability(dr, Keff.p.ref, Keff.p.2)
 	m.ID[P] = ID.p.2
-  m.lalign[P] = nalign 
-  m.nH[P,1:nmodes] = t(VA$nH)
-  m.Pn[P,1:nmodes] = t(VA$Pn)
-  m.d.evalues[P,1:nmodes]  = t(VA$d.evalues[1:nmodes])
-	m.evalues[P,1:nmodes]  = t(Keff.p.ref$va[1:nmodes])
-	dr.squarei = rbind(VA$dr.squarei,align.p.ref.index)
+  m.laligned[P] = naligned 
+  m.nH[P, 1:nmodes] = t(VA$nH)
+  m.Pn[P, 1:nmodes] = t(VA$Pn)
+  m.d.evalues[P, 1:nmodes]  = t(VA$d.evalues[1:nmodes])
+	m.evalues[P, 1:nmodes]  = t(Keff.p.ref$va[1:nmodes])
+	dr.squarei = rbind(VA$dr.squarei, aligned.p.ref.index)
 	for (i in (1:naa.p.ref)){
-		m.dr.squarei[P,i]= matrix(dr.squarei[1,dr.squarei[2,]==i],ncol=1,nrow=1)
+		m.dr.squarei[P, i] = matrix(dr.squarei[1, dr.squarei[2, ] == i], ncol = 1, nrow = 1)
 	}
 }
-
 P.index = (pdbids != as.character(p.ref))
-
-m.ID <- m.ID[P.index,]
-m.lalign <- m.lalign[P.index,]
-m.nH <- m.nH[P.index,]
-m.Pn <- m.Pn[P.index,]
-m.d.evalues <- m.d.evalues[P.index,]
-m.evalues <- m.evalues[P.index,]
-m.dr.squarei <- m.dr.squarei[P.index,]
-
-#Calculate means#
-mean.nH = colMeans(m.nH,na.rm=T)
-mean.Pn = colMeans(m.Pn,na.rm=T)
-mean.d.evalues= colMeans(m.d.evalues,na.rm=T)
-mean.evalues= colMeans(m.evalues,na.rm=T)
-MSDi = colMeans(m.dr.squarei,na.rm = T)
-MSD = rowMeans(m.dr.squarei,na.rm = T)
-
-#SAVE INFORMATION#
-if (family == "Globins") family <- paste(family,"Heme",heme,sep ="")
-
-#Dataframes#
-write.csv(m.lalign,file = file.path(OUT , paste(family , ".Out.m.lalign.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.ID,file = file.path(OUT, paste(family , ".Out.m.ID.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.nH,file = file.path(OUT , paste(family , ".Out.m.nH.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.Pn,file = file.path(OUT , paste(family , ".Out.m.Pn.csv" , sep="")) , row.names = FALSE)
-write.csv(m.d.evalues,file = file.path(OUT , paste(family , ".Out.m.d.evalues.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.evalues,file = file.path(OUT , paste(family , ".Out.m.evalues.csv" , sep = "")), row.names = FALSE)
-write.csv(m.dr.squarei,file = file.path(OUT , paste(family , ".Out.m.dr.squarei.csv" , sep = "")) , row.names = FALSE)
-
-#Means#
-write.csv(mean.nH,file = file.path(OUT , paste(family , ".Out.nH.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(mean.Pn,file = file.path(OUT , paste(family , ".Out.Pn.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(mean.d.evalues,file = file.path(OUT , paste(family , ".Out.d.evalues.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(mean.evalues,file = file.path(OUT , paste(family , ".Out.evalues.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(MSDi,file = file.path(OUT , paste(family , ".Out.MSDi.csv" , sep = "")) , row.names = FALSE)
-write.csv(MSD,file = file.path(OUT , paste(family , ".Out.MSD.csv", sep = "")) , row.names = FALSE)
-
-#input#
-write.csv(input,file = file.path(OUT , paste(family , ".Input" , sep = "")) , row.names = FALSE)
+m.ID <- m.ID[P.index, ]
+m.laligned <- m.laligned[P.index, ]
+m.nH <- m.nH[P.index, ]
+m.Pn <- m.Pn[P.index, ]
+m.d.evalues <- m.d.evalues[P.index, ]
+m.evalues <- m.evalues[P.index, ]
+m.dr.squarei <- m.dr.squarei[P.index, ]
+# Calculate means.
+mean.nH = colMeans(m.nH, na.rm = T)
+mean.Pn = colMeans(m.Pn, na.rm = T)
+mean.d.evalues= colMeans(m.d.evalues, na.rm = T)
+mean.evalues= colMeans(m.evalues, na.rm = T)
+MSDi = colMeans(m.dr.squarei, na.rm = T)
+MSD = rowMeans(m.dr.squarei, na.rm = T)
+# Save information.
+if (family == "globins") {
+  family <- paste(family, "_heme_", heme, sep = "")
+}
+write.csv(m.laligned, file = file.path(out.dir, paste(family, "_out_m.laligned.csv", sep = "")), row.names = FALSE)
+write.csv(m.ID, file = file.path(out.dir, paste(family, "_out_m.ID.csv", sep = "")), row.names = FALSE)
+write.csv(m.nH, file = file.path(out.dir, paste(family, "_out_m.nH.csv", sep = "")), row.names = FALSE)
+write.csv(m.Pn, file = file.path(out.dir, paste(family, "_out_m.Pn.csv", sep = "")), row.names = FALSE)
+write.csv(m.d.evalues, file = file.path(out.dir, paste(family, "_out_m.d.evalues.csv", sep = "")), row.names = FALSE)
+write.csv(m.evalues, file = file.path(out.dir, paste(family, "_out_m.evalues.csv", sep = "")), row.names = FALSE)
+write.csv(m.dr.squarei, file = file.path(out.dir, paste(family, "_out_m.dr.squarei.csv", sep = "")), row.names = FALSE)
+write.csv(mean.nH, file = file.path(out.dir, paste(family, "_out_nH.mean.csv", sep = "")), row.names = FALSE)
+write.csv(mean.Pn, file = file.path(out.dir, paste(family, "_out_Pn.mean.csv", sep = "")), row.names = FALSE)
+write.csv(mean.d.evalues, file = file.path(out.dir, paste(family, "_out_d.evalues.mean.csv", sep = "")), row.names = FALSE)
+write.csv(mean.evalues, file = file.path(out.dir, paste(family, "_out_evalues.mean.csv", sep = "")), row.names = FALSE)
+write.csv(MSDi, file = file.path(out.dir, paste(family, "_out_MSDi.csv", sep = "")), row.names = FALSE)
+write.csv(MSD, file = file.path(out.dir, paste(family , "_out_MSD.csv", sep = "")), row.names = FALSE)
+write.csv(input, file = file.path(out.dir, paste(family , "_input", sep = "")), row.names = FALSE)
--- TheoreticalMain.R
+++ TheoreticalMain.R
@@ -5,10 +5,10 @@
 # entre sí. Además, calcula medidas de variabilidad estructural y dinámica entre las proteínas mutantes 
 # generadas y la proteína de referencia.
 #
-# Para utilizar el programa se debe completar un input ("DATA/Theoretical/inputT.csv") especificando: 
-# -family: la familia de proteínas a la que pertenece la proteína a mutar escrita en inglés y plural 
-# (ej.:"Globins"). Tener en cuenta que las familias que pueden ser analizadas experimentalmente son
-# "SerinProteases", "Globins" o "Plastocyanins".
+# Para utilizar el programa se debe completar un input ("DATA/Theoretical/theoretical_input.csv") especificando: 
+# -family: la familia de proteínas a la que pertenece la proteína a mutar. 
+# Tener en cuenta que las familias que pueden ser analizadas experimentalmente son
+# "serinProteases", "globins" o "plastocyanins".
 # -p.ref: el código de pdb (pdbid) de la proteína a mutar (ej.:"1a6m")
 # -chain: la cadena de p.ref que se desea mutar.
 # -heme: solo se utiliza para la familia de las globinas, puede ser "TRUE" o "FALSE" dependiendo de 
@@ -18,164 +18,129 @@
 # -Fmax: valor máximo de las fuerzas usadas para la simulación de las mutaciones.
 # -model: el modelo de red elástica a usar, que solo puede ser "ANM" por el momento.
 # -R0: cut-off del ANM.
-
-#Remove objects#
+# Remove objects.
 rm(list = ls()) 
-
-#Read input#
+# Read input.
 data.dir <- "DATA/Theoretical"
-
-input.dir <- file.path(data.dir,"InputT.csv")
-input <- read.csv(input.dir)
-
-#Reference family and protein#
+input.fname <- file.path(data.dir, "theoretical_input.csv")
+input <- read.csv(input.fname)
 family <- input$family
 p.ref <- input$p.ref 
 chain <- input$chain
 heme <- input$heme 
-
-#Parameters for mutations#
 nmut = input$nmut 
 nsitesmut = input$nsitesmut 
 fmax = input$fmax 
-
-#ENM#
 model <- input$model
 R0 = input$R0
-
-#PDB dir#
-get.pdb(as.character(p.ref),data.dir) 
-pdb.fname <- file.path(data.dir,paste(p.ref,".pdb",sep=""))
-
-#Functions dir#
-f.readCA <-"FUNCTIONS/ReadCA.R" 
-f.readHeme <- "FUNCTIONS/ReadHeme.R"  
-f.Keff <- "FUNCTIONS/Keff.R"  
-f.K <- "FUNCTIONS/K.R" 
-f.kij.function <- file.path("FUNCTIONS",paste("Kij.",model,".R",sep = "")) 
-f.force <- "FUNCTIONS/Force.R"  
-f.variability <- "FUNCTIONS/Variability.R"  
-
-#Output dir#
-OUT <- "OUT/Theoretical"
-
-#General parameters#
+# PDB fname.
+get.pdb(as.character(p.ref), data.dir) 
+pdb.fname <- file.path(data.dir, paste(p.ref, ".pdb", sep = ""))
+# Functions fnames.
+ReadCA.fname <- "FUNCTIONS/ReadCA.R" 
+ReadHeme.fname <- "FUNCTIONS/ReadHeme.R"  
+CalculateKeff.fname <- "FUNCTIONS/CalculateKeff.R"  
+CalculateK.fname <- "FUNCTIONS/CalculateK.R" 
+CalculateKij.fname <- file.path("FUNCTIONS", paste("CalculateKij", model, ".R", sep = "")) 
+CalculateForce.fname <- "FUNCTIONS/CalculateForce.R"  
+CalculateVariability.fname <- "FUNCTIONS/CalculateVariability.R"
+# Output dir.
+out.dir <- "OUT/Theoretical"
+# General parameters.
 TOLERANCE = 1e-10
-
-#Load librarys#
+# Load librarys.
 library(bio3d)
-
-#Load Functions#
-source(f.readCA) 
-source(f.readHeme) 
-source(f.Keff)
-source(f.K)
-source(f.kij.function)
-source(f.force)
-source(f.variability)
-
-#Read PDB of p.ref#
-pdb <- readCA(pdb.fname,chain)
+# Source functions.
+source(ReadCA.fname)
+source(ReadHeme.fname)
+source(CalculateKeff.fname)
+source(CalculateK.fname)
+source(CalculateKij.fname)
+source(CalculateForce.fname)
+source(CalculateVariability.fname)
+# Read PDB of p.ref.
+pdb <- ReadCA(pdb.fname, chain)
 r.p.ref = pdb$xyz.calpha
 nsites = pdb$nsites
-naa = nsites 
-
-if ((family == "Globins") & (heme == "TRUE")){
-  r.heme = readHeme(pdb.fname,chain)
-  r.p.ref = cbind(r.p.ref,r.heme)
+naa = nsites  # naa = n.aminoaids, nsites = n.aminoácids + n.atoms we want to consider, ej.: Ns and Fe from heme group in Globins# 
+if (family == "globins" & heme == "TRUE") {
+  r.heme = ReadHeme(pdb.fname, chain)
+  r.p.ref = cbind(r.p.ref, r.heme)
   nsites = ncol(r.p.ref)
-  align.index = seq(1,naa)
-  no.align.index = seq((naa+1),nsites)
+  aligned.index = seq(1, naa)
+  not.aligned.index = seq((naa+1), nsites)
 }
-
-#Calculate K of p.ref#
-K.p.ref <- K(r.p.ref,kij.function,R0,TOLERANCE)
-
-#Variability#
-m.nH = matrix(ncol = 3*naa , nrow = nmut)
-m.Pn = matrix(ncol = 3*naa , nrow = nmut)
-m.d.evalues = matrix(ncol = 3*naa , nrow = nmut)
-m.dr.squarei = matrix(ncol = naa , nrow = nmut)
-
-#Count the number of mutants to discard#
+# Calculate K of p.ref.
+K.p.ref <- CalculateK(r.p.ref, CalculateKij, R0, TOLERANCE)
+# Measures of variability.
+m.nH = matrix(ncol = 3 * naa, nrow = nmut)
+m.Pn = matrix(ncol = 3 * naa, nrow = nmut)
+m.d.evalues = matrix(ncol = 3 * naa, nrow = nmut)
+m.dr.squarei = matrix(ncol = naa, nrow = nmut)
+# Count the number of mutants to discard.
 count = 0
-
-#Calculate mutants#
-for(mut in seq(nmut)){
- 	f <- rep(0,3*nsites)
-	for (l in (sample(1:naa,replace = F)[1:nsitesmut])){
-    print(c(mut,l))
-    fl = force(l,r.p.ref,K.p.ref$kij,fmax)
-		f = f + fl
-	}
-
-  dr = K.p.ref$Cov %*% f
-  dim(dr) <- c(3,nsites)
+# Calculate mutants.
+for(mut in seq(nmut)) {
+  f <- rep(0, 3 * nsites)
+  for (l in (sample(1:naa, replace = F)[1:nsitesmut])) {
+    print(c(mut, l))
+    fl = CalculateForce(l, r.p.ref, K.p.ref$kij, fmax)
+    f = f + fl
+  }
+  dr = K.p.ref$cov %*% f
+  dim(dr) <- c(3, nsites)
   r.mut = r.p.ref + dr
-
-  #Rotate mutants minimizing RMSD with P.ref#
-	r.mut <- matrix(fit.xyz(fixed = as.vector(r.p.ref), 
-	mobile = as.vector(r.mut),
-	fixed.inds = seq(1,3*naa),
-	mobile.inds = seq(1,3*naa)),nrow=3) 
-	
-	#Calculate K of mutants and p.ref#
+  # Rotate mutants minimizing RMSD with P.ref.
+  r.mut <- matrix(fit.xyz(fixed = as.vector(r.p.ref), 
+                         mobile = as.vector(r.mut),
+                     fixed.inds = seq(1, 3 * naa),
+                    mobile.inds = seq(1, 3 * naa)), nrow = 3) 
+  # Calculate K of mutants and p.ref.
   if (naa != nsites) {
-    K.mut <- keff(r.mut, align.index,no.align.index, kij.function,R0,TOLERANCE)
-    K.p.ref.2 <- keff(r.p.ref,align.index,no.align.index,kij.function,R0,TOLERANCE)
+    K.mut <- CalculateKeff(r.mut, aligned.index, not.aligned.index, CalculateKij, R0, TOLERANCE)
+    K.p.ref.2 <- CalculateKeff(r.p.ref, aligned.index, not.aligned.index, CalculateKij, R0, TOLERANCE)
   }
   if (naa == nsites) {
-    K.mut <- K(r.mut,kij.function,R0,TOLERANCE)
+    K.mut <- CalculateK(r.mut, CalculateKij, R0, TOLERANCE)
     K.p.ref.2 <- K.p.ref
   }
-	
-	nmodes <- length(K.p.ref.2$va)  
-	
-  #Calculate dr#
-  dr = (r.mut - r.p.ref)[,1:naa]   
-  
-	if (ncol(K.mut$ve) == ncol(K.p.ref.2$ve)){ 
-	  
-	  #Calculate measures o variability#
-		VA <- variability(dr,K.p.ref.2,K.mut)
-    m.nH[mut,1:nmodes] = t(VA$nH)
-  	m.Pn[mut,1:nmodes] = t(VA$Pn)
-    m.d.evalues[mut,1:nmodes]  = t(VA$d.evalues[1:nmodes])
-		dr.squarei = rbind(VA$dr.squarei,seq(1,naa))
-		for (i in (1:naa)){
-			m.dr.squarei[mut,i]= matrix(dr.squarei[1,dr.squarei[2,]==i],ncol=1,nrow=1)
-		}
-	}
-	if (ncol(K.mut$ve) != ncol(K.p.ref.2$ve)){ 
-	  count <- count +1
-	}
+  nmodes.p.ref.2 <- length(K.p.ref.2$va)  
+  nmodes.mut <- length(K.mut$va)  
+  # Calculate dr.
+  dr = (r.mut - r.p.ref)[, 1:naa]   
+  # Calculate measures o variability.
+  if (nmodes.p.ref.2 == nmodes.mut) { 
+    VA <- CalculateVariability(dr, K.p.ref.2, K.mut)
+    m.nH[mut, 1:nmodes.p.ref.2] = t(VA$nH)
+    m.Pn[mut, 1:nmodes.p.ref.2] = t(VA$Pn)
+    m.d.evalues[mut, 1:nmodes.p.ref.2]  = t(VA$d.evalues[1:nmodes.p.ref.2])
+    dr.squarei = rbind(VA$dr.squarei, seq(1, naa))
+    for (i in (1:naa)) {
+      m.dr.squarei[mut, i] = matrix(dr.squarei[1, dr.squarei[2, ] == i], ncol = 1, nrow = 1)
+    }
+  }
+  if (nmodes.p.ref.2 != nmodes.mut) { 
+    count <- count +1
+  }
 }
-
-#Calculate Means#
-mean.nH = colMeans(as.matrix(m.nH),na.rm=T)
-mean.Pn = colMeans(as.matrix(m.Pn),na.rm=T)
-mean.d.evalues= colMeans(as.matrix(m.d.evalues),na.rm=T)
-MSDi = colMeans(as.matrix(m.dr.squarei),na.rm=T)
-MSD = rowMeans(m.dr.squarei,na.rm = T)
-
-#SAVE INFORMATION#
-if (family == "Globins") family <- paste(family,"Heme",heme,sep ="")
-
-#Dataframes#
-write.csv(m.nH,file = file.path(OUT , paste(family , ".Out.m.nH.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.Pn,file = file.path(OUT , paste(family , ".Out.m.Pn.csv" , sep="")) , row.names = FALSE)
-write.csv(m.d.evalues,file = file.path(OUT , paste(family , ".Out.m.d.evalues.csv" , sep = "")) , row.names = FALSE)
-write.csv(m.dr.squarei,file = file.path(OUT , paste(family , ".Out.m.dr.squarei.csv" , sep = "")) , row.names = FALSE)
-write.csv(K.p.ref.2$va,file = file.path(OUT , paste(family , ".Out.evalues.csv" , sep = "")) , row.names = FALSE)
-
-#Means#
-write.csv(mean.nH,file = file.path(OUT , paste(family , ".Out.nH.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(mean.Pn,file = file.path(OUT , paste(family , ".Out.Pn.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(mean.d.evalues,file = file.path(OUT , paste(family , ".Out.d.evalues.mean.csv" , sep = "")) , row.names = FALSE)
-write.csv(MSDi,file = file.path(OUT , paste(family , ".Out.MSDi.csv" , sep = "")) , row.names = FALSE)
-write.csv(MSD,file = file.path(OUT , paste(family , ".Out.MSD.csv", sep = "")) , row.names = FALSE)
-
-#input#
-write.csv(input,file = file.path(OUT , paste(family , ".Input" , sep = "")) , row.names = FALSE)
-
-
+# Calculate Means.
+mean.nH = colMeans(as.matrix(m.nH), na.rm = T)
+mean.Pn = colMeans(as.matrix(m.Pn), na.rm = T)
+mean.d.evalues = colMeans(as.matrix(m.d.evalues), na.rm = T)
+MSDi = colMeans(as.matrix(m.dr.squarei), na.rm = T)
+MSD = rowMeans(m.dr.squarei, na.rm = T)
+# Save information.
+if (family == "globins") {
+  family <- paste(family, "_heme_", heme, sep = "")
+}
+write.csv(m.nH, file = file.path(out.dir, paste(family, "_out_m.nH.csv", sep = "")), row.names = FALSE)
+write.csv(m.Pn, file = file.path(out.dir, paste(family, "_out_m.Pn.csv", sep = "")), row.names = FALSE)
+write.csv(m.d.evalues, file = file.path(out.dir, paste(family, "_out_m.d.evalues.csv", sep = "")), row.names = FALSE)
+write.csv(m.dr.squarei, file = file.path(out.dir, paste(family, "_out_m.dr.squarei.csv", sep = "")), row.names = FALSE)
+write.csv(K.p.ref.2$va, file = file.path(out.dir, paste(family, "_out_evalues.csv", sep = "")), row.names = FALSE)
+write.csv(mean.nH, file = file.path(out.dir, paste(family, "_out_nH.mean.csv", sep = "")), row.names = FALSE)
+write.csv(mean.Pn, file = file.path(out.dir, paste(family, "_out_Pn.mean.csv", sep = "")), row.names = FALSE)
+write.csv(mean.d.evalues, file = file.path(out.dir, paste(family, "_out_d.evalues.mean.csv", sep = "")), row.names = FALSE)
+write.csv(MSDi, file = file.path(out.dir, paste(family, "_out_MSDi.csv", sep = "")), row.names = FALSE)
+write.csv(MSD, file = file.path(out.dir, paste(family, "_out_MSD.csv", sep = "")), row.names = FALSE)
+write.csv(input, file = file.path(out.dir, paste(family, "_input", sep = "")), row.names = FALSE)
